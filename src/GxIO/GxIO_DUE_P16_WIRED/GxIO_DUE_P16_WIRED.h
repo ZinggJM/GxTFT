eadd822f3530f4869c2f6be75f3197560502ca83
Matveev Dmitriy <randomnt@gmail.com>
2018-11-04 00:17:34 +0300

    minor patch to works with OTM8009A on DUE board

--- src/GxIO/GxIO_DUE_P16_WIRED/GxIO_DUE_P16_WIRED.h
+++ src/GxIO/GxIO_DUE_P16_WIRED/GxIO_DUE_P16_WIRED.h
@@ -3,6 +3,8 @@
 // License: GNU GENERAL PUBLIC LICENSE V3, see LICENSE
 //
 // for 7inch display https://www.aliexpress.com/item/New-7-inch-TFT-LCD-module-800x480-SSD1963-Touch-PWM-For-Arduino-AVR-STM32-ARM/32667404985.html
+//
+// and IPS 3.97inch OTM8009A https://www.aliexpress.com/item/IPS-3-97-inch-HD-TFT-LCD-Touch-Screen-Module-OTM8009A-Drive-IC-800-480/32676929794.html
 
 #ifndef _GxIO_DUE_P16_WIRED_H_
 #define _GxIO_DUE_P16_WIRED_H_
@@ -24,10 +26,12 @@ class GxIO_DUE_P16_WIRED : public GxIO
     uint16_t readData16();
     uint32_t readRawData32(uint8_t part); // debug purpose
     void writeCommandTransaction(uint8_t c);
-    void writeDataTransaction(uint8_t d);
+	void writeCommand16Transaction(uint16_t c);
+	void writeDataTransaction(uint8_t d);
     void writeData16Transaction(uint16_t d, uint32_t num = 1);
     void writeCommand(uint8_t c);
-    void writeData(uint8_t d);
+	void writeCommand16(uint16_t c);
+	void writeData(uint8_t d);
     void writeData(uint8_t* d, uint32_t num);
     void writeData16(uint16_t d, uint32_t num = 1);
     void writeAddrMSBfirst(uint16_t d);
